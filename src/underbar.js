d046a37fc003b0ccf7d3b164a97b2e34358a5913
Manuela Gomez <mmanu.gomez@gmail.com>
2019-06-15 21:12:32 -0400

    Fixed _.delay to pass the arguments to the function

--- src/underbar.js
+++ src/underbar.js
@@ -330,8 +330,9 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    let args = Array.prototype.slice.call(arguments,2)
     return setTimeout(function() {
-      return func.apply(null,arg);
+      return func.apply(this,args);
     },wait);
   };
 
